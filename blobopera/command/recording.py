83111fd235c4e39d33511783f0eb815c684545f0
Helio Machado <0x2b3bfa0+git@googlemail.com>
2021-12-26 01:33:27 +0100

    Update recording.py

--- blobopera/command/recording.py
+++ blobopera/command/recording.py
@@ -52,7 +52,7 @@ def upload(
     IDENTIFIER: displays the raw recording identifier as
     returned by the server.
 
-    LINK: displays a full link to the recording player, inluding the encoded
+    LINK: displays a full link to the recording player, including the encoded
     recording identifier as a parameter.
 
     SHORT: displays a shortened version of the aforementioned link.
