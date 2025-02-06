d59aa2fede5f970a18b68c234d7b0a5b410240ab
Helio Machado <0x2b3bfa0+git@googlemail.com>
2025-02-06 15:02:17 +0100

    Update test_language_random.py

--- tests/test_language_random.py
+++ tests/test_language_random.py
@@ -1,6 +1,11 @@
 import music21  # type: ignore
 import pytest  # type: ignore
 
+from blobopera.languages import GenericLanguage, RandomLanguage
+from blobopera.phoneme import Phoneme
+
+from .fixture_note_lyric_events import foo_events, foo_none_events
+
 from blobopera.languages import RandomLanguage
 
 
