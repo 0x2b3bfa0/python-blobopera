c83dbc3c3800f20d098c9cfdee47309c286ee8b7
Helio Machado <0x2b3bfa0+git@googlemail.com>
2025-02-06 15:05:14 +0100

    Update test_language_random.py

--- tests/test_language_random.py
+++ tests/test_language_random.py
@@ -1,13 +1,10 @@
 import music21  # type: ignore
 import pytest  # type: ignore
 
-from blobopera.languages import GenericLanguage, RandomLanguage
+from blobopera.languages import RandomLanguage
 from blobopera.phoneme import Phoneme
 
 from .fixture_note_lyric_events import foo_events, foo_none_events
-
-from blobopera.languages import RandomLanguage
-
 
 def test_random_language(foo_none_events):
     language = RandomLanguage(music21.stream.Part(), strict=False)
